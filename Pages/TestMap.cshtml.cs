c8875d2776379ba0eb8debbf2caf0868d4564d46
MathiasNoergaard <man010@edu.zealand.dk>
2023-11-16 13:44:24 +0100

    Changes

---
+++ Pages/TestMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigMammaWebsite.Pages
+{
+    public class TestMapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
